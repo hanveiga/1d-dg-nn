c4266fad8b1de2851b81711f46e171d35b745ef2
hanveiga <han.veiga@gmail.com>
2018-02-08 13:48:51 +0100

    parameters for nn

---
+++ neural net/parameters.py
@@ -0,0 +1,18 @@
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 128 # 2nd layer number of neurons
+n_hidden_3 = 64 # 3th layer number of neurons
+n_hidden_4 = 32 # 4th layer number of neurons
+num_input = 8 # inputs  for features
+num_classes = 2 # binary classification
+
+LEARNING_RATE = 0.000005
+num_steps = 25000
+batch_size = 128
+display_step = 1000
+
+dataset_path = 'dataset07.2.18.csv'
+identifier = '_7.2.18_'
+label_column = 9 # 10th
+start_f = 1
+end_f = 9
